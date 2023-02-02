ccdc2e4a7f98b7534eb55efcf4b40a733e1f92ff
lingdeyiyicifang <31645840+lingdeyiyicifang@users.noreply.github.com>
2023-02-02 21:14:37 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: '/running_page', // Change to `/running_page` when running on github pages
+  pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
     siteTitle: 'Zzz的跑步生涯',
     siteUrl: 'https://lingdeyiyicifang.github.io/running_page/',
